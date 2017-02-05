e21a22b62d958e251eac88df9d5f4309db42320e
Kevin Zuern <kevin@kevinzuern.com>
2017-02-05 06:22:37 -0500

    Add slider persistence

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,22 @@
 function Food(displayName, percentRemaining, knownAliases, id, rev) {
 	var self = this;
 	self.displayName = ko.observable(displayName);
-  	self.percentRemaining = ko.observable(percentRemaining);
-  	self.knownAliases = ko.observableArray(knownAliases);
-  	self._id = ko.observable(id);
-  	self._rev = ko.observable(rev);
+	self.percentRemaining = ko.observable(percentRemaining);
+	self.knownAliases = ko.observableArray(knownAliases);
+	self._id = ko.observable(id);
+	self._rev = ko.observable(rev);
+
+	self.withoutObservables = function() {
+		return {
+			displayName: self.displayName(),
+			percentRemaining: self.percentRemaining(),
+			knownAliases: self.knownAliases(),
+			_id: self._id(),
+			_rev: self._rev()
+		};
+	}
 }
+
 function GroceryViewModel() {
 	var self = this;
 	self.items = ko.observableArray([]);
@@ -38,10 +49,43 @@ function GroceryViewModel() {
 			console.log("itemList loaded")
 		});
 	};
+}
 
-};
+/**
+ * Update the specified item on the server
+ * @param {itemId:GUID} - The id of the item to update
+ */
+function updateItem(itemId) {
+	// Find the item
+	var updatedItem;
+	viewModel.items().forEach(function(item) {
+		if (item._id() === itemId)
+			updatedItem = item;
+	});
+
+	if (!updatedItem) {
+		console.log("Item was not found");
+		return;
+	}
+
+	// Prep item to update
+	updatedItem = updatedItem.withoutObservables();
+
+	// Post to the server
+	$.post('/api/items/', updatedItem)
+	.fail(function(err){
+		console.log("Failed to push updated item to the server");
+		console.log(err);
+	});
+}
+
+function onSliderChanged(e) {
+	var itemId = $(e.currentTarget).data('item-id');
+	updateItem(itemId);
+}
 
 var viewModel = new GroceryViewModel();
 ko.applyBindings(viewModel);
 viewModel.startup();
 
+$(document).on('change', '.percentSlider', onSliderChanged);
