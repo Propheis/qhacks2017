3cf6cb83565d4deac39c6d3316f34e7b6c3b0c3f
Deven Bernard <deven.bernard@gmail.com>
2017-02-05 03:48:52 -0500

    Revert "focus binds and search bar"
    This reverts commit 6c224cf286b10d7455a2deb38ddbc04422f65b52.

--- public/scripts/recipescript.js
+++ public/scripts/recipescript.js
@@ -1,23 +1,24 @@
 function RecipeViewModel() {
 	var self = this;
-	self.recipes = ko.observableArray([]);
-	self.searchText = ko.observable("");
-	self.keywordList = ko.computed(function(){
-		return self.searchText().split(" ");
-	});
-	self.searchRecipes = function (Recipe){
-
-			$.post("/api/recipes", {'': self.keywordList()}, function (infoList){
+	self.recipes = ko.observableArray([])
+	self.init = function (Recipe){
+		
+		$.get("/api/items", null, function (itemsList){
+			var keywordList = [];
+			itemsList.forEach(function (item){
+				keywordList.push(item.displayName);
+			});
+			$.post("/api/recipes", {'': keywordList}, function (infoList){
 				self.recipes(infoList);
 				console.log("Recipes added");
 			})
 			.fail(function(err) {
 				console.log(err);
 			});
+		});
 	};
-}
+};
+
 var viewModel = new RecipeViewModel();
 ko.applyBindings(viewModel);
-$(document).ready(function(){
-	$("#searchBar").focus();
-});
+viewModel.init();
